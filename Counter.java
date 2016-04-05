52a67b2d3df7bc90671c8f76e2253e26dc39c96f
Caio Coelho <caiovico@gmail.com>
2016-04-04 21:04:54 -0300

    inicio

---
+++ Counter.java
@@ -0,0 +1,15 @@
+class Team implements Comparable{
+	private String name;
+	private int points;
+	private int victories;
+	private int balance;
+
+
+}
+
+public class Counter{
+	static Set<Team> teams = new Map<>();
+	public static void getGame(String s){
+
+	}
+}
