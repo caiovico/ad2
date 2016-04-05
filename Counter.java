89f434b60f43c431fee6984dfe6b6f28eae9a5cb
Caio Coelho <caiovico@gmail.com>
2016-04-05 13:41:45 -0300

    test LeagueGame

--- Counter.java
+++ Counter.java
@@ -1,4 +1,5 @@
-class VolleyTeam implements Comparable<Team>{
+import java.util.*;
+class VolleyTeam implements Comparable<VolleyTeam>{
 	private String name;
 	private int points;
 	private int victories;
@@ -8,15 +9,15 @@ class VolleyTeam implements Comparable<Team>{
 		this.name=s;
 	}
 
-	public addPoints(int i){
+	public void addPoints(int i){
 		this.points+=i;
 	}
 
-	public addVictory(){
-		victories++
+	public void addVictory(){
+		victories++;
 	}
 
-	public changeBalance(int i){
+	public void changeBalance(int i){
 		balance+=i;
 	}
 
@@ -25,20 +26,20 @@ class VolleyTeam implements Comparable<Team>{
 	}
 
 	public boolean equals(Object o){
-		if (o instanceof Team){
-			Team t = (Team)o;
+		if (o instanceof VolleyTeam){
+			VolleyTeam t = (VolleyTeam)o;
 			return this.name.equals(t.name);
 		}else return false;
 	}
 
-	public int compareTo(Team t){
+	public int compareTo(VolleyTeam t){
 		//compares in descending order, the first has more points, more victories and/or more balance
-		if (this.points!=t.points) return t.point-this.points;
+		if (this.points!=t.points) return t.points-this.points;
 		if (this.victories!=t.victories) return t.victories-this.points;
 		return t.victories-this.victories;
 	}
 
-	public Team merge(Team t){
+	public VolleyTeam merge(VolleyTeam t){
 		this.victories+=t.victories;
 		this.addPoints(t.points);
 		this.changeBalance(t.balance);
@@ -49,19 +50,33 @@ class VolleyTeam implements Comparable<Team>{
 
 class LeagueGame{
 	VolleyTeam home, visitor;
+	String homeName, visitorName;
 	public LeagueGame(String s){
-		Scanner sc = new Scanner(s);
-		String homeName = "";
-		while (sc.next!="vs"){
-			
+		String[] tokens = s.split("vs");
+		homeName = tokens[0].trim();
+		tokens = tokens[1].split("/");
+		visitorName = tokens[0].trim();
+
+		for (int i = 1; i<tokens.length;i++){
+			System.out.println(tokens[i]);
 		}
+
+
+		System.out.println(homeName);
+		System.out.println(visitorName);
+
 	}
 }
 
 public class Counter{
 
-	static Set<VolleyTeam> teams = new TreeSet<>();
-	public static void getGame(String s){
+	public static void main(String[] args){
+		LeagueGame lg1 = new LeagueGame("DENTIL-PRAIA CLUBE vs CAMPONESA-MINAS/3-2/22-25/25-15/21-25/25-22/15­-12");
+		//LeagueGame lg2 = new LeagueGame("DENTIL-PRAIA CLUBE vs CAMPONESA-MINAS/3­-0/25­-21/25­-18/25­-22");
+
+		//System.out.println(lg1.homeName.equals(lg2.homeName));
+
+
 
 	}
 }
