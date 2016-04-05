962380accd278d81c2036d109b669084b5440a38
Caio Coelho <caiovico@gmail.com>
2016-04-05 01:54:09 -0300

    compareTo

--- Counter.java
+++ Counter.java
@@ -1,8 +1,18 @@
-class Team implements Comparable<Team>{
+class VolleyTeam implements Comparable<Team>{
 	private String name;
 	private int points;
 	private int victories;
 	private int balance;
+
+	public addPoints(int i){
+		this.points+=i;
+	}
+
+	public addVictory(){
+		victories++
+	}
+
+	public setBalance(){}
 
 	public String toString(){
 		return this.name;
@@ -16,13 +26,28 @@ class Team implements Comparable<Team>{
 	}
 
 	public int compareTo(Team t){
-
+		//compares in descending order, the first has more points, more victories and/or more balance
+		if (this.points!=t.points) return t.point-this.points;
+		if (this.victories!=t.victories) return t.victories-this.points;
+		if(this.balance!=t.balance) return t.victories-this.victories;
+		return 0;
 	}
+
+	public Team merge(Team t){
+		this.addVictory(t.victories);
+		this.addPoints(t.points);
+		this.changeBalance(t.balance);
+	}
+}
+
+
+class LeagueGame{
+
 }
 
 public class Counter{
 
-	static Set<Team> teams = new TreeSet<>();
+	static Set<VolleyTeam> teams = new TreeSet<>();
 	public static void getGame(String s){
 
 	}
