74a870e79fde418a084e6369c2eb3c3a2c9ceff2
Caio Coelho <caiovico@gmail.com>
2016-04-04 22:19:20 -0300

    simple equals

--- Counter.java
+++ Counter.java
@@ -1,14 +1,28 @@
-class Team implements Comparable{
+class Team implements Comparable<Team>{
 	private String name;
 	private int points;
 	private int victories;
 	private int balance;
 
+	public String toString(){
+		return this.name;
+	}
 
+	public boolean equals(Object o){
+		if (o instanceof Team){
+			Team t = (Team)o;
+			return this.name.equals(t.name);
+		}else return false;
+	}
+
+	public int compareTo(Team t){
+
+	}
 }
 
 public class Counter{
-	static Set<Team> teams = new Map<>();
+
+	static Set<Team> teams = new TreeSet<>();
 	public static void getGame(String s){
 
 	}
